fc6f5874568e6186067c35fdbc92c304908f88bc
cizixiu <171167601@qq.com>
2019-06-02 09:19:54 +0800

    1

---
+++ demo/js/shici.js
@@ -0,0 +1,23 @@
+
+    jinrishici.load(function(result) {
+        $("#jinrishici-sentence-user").text();
+        $("#jinrishici-sentence-user").html(		
+			
+             "<ul>" +
+			        
+			"<li><strong>诗 词：</strong>"+result.data.content+"</li>" +
+			        "<li>《"+result.data.origin.title+"》</li>" +
+						"<li>"+result.data.origin.author+"</li>" +
+						"<li>"+result.data.origin.dynasty+"</li>" +
+						
+					"</ul>"
+			        );
+    });
+    jinrishici.load(function(result) {
+        $("#jinrishici-s").text("jinrishici");
+        $("#jinrishici-s").html(						        
+			"<span>"+result.data.content+"</span>"+
+			"<span>——"+result.data.origin.author+"（"+result.data.origin.dynasty+"）"+"</span>"
+
+			        );
+    });
