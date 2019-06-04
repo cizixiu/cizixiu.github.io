485bc56955c5230bd2277c69786e81a7f090e22e
cizixiu <171167601@qq.com>
2019-06-04 12:02:01 +0800

    0

---
+++ shici/js/shici.js
@@ -0,0 +1,43 @@
+
+
+jinrishici.load(function(result) {
+        
+        $("#jinrishici-sentence-user").html(		
+			
+             "<ul>" +	
+			        "<li>经典句子："+result.data.content+"</li>" +	
+			        "<li>诗词名字：《"+result.data.origin.title+"》</li>" +
+						"<li>诗词作者："+result.data.origin.author+"</li>" +
+						"<li>作者时代："+result.data.origin.dynasty+"</li>" +
+					"</ul>"+	
+			"<div >诗词全文："+result.data.origin.content+"</div>" 
+			        );
+    });
+    jinrishici.load(function(result) {
+       
+        $("#jinrishici-s").html(						        
+			"<span>"+result.data.content+"</span>"+
+			"<span>——"+result.data.origin.author+"（"+result.data.origin.dynasty+"）"+"</span>"
+
+			        );
+    });
+    jinrishici.load(function(result) {
+        
+        $("#jinrishici-sentence-quanwen").html(		
+			"<div class='s-title'>《"+result.data.origin.title+"》</div>" +
+			"<div class='s-author'>"+result.data.origin.author+"("+result.data.origin.dynasty+")" +"</div>" +
+			
+			"<div class='s-content'>"+result.data.origin.content+"</div>" 
+			        );
+	 var break_line=document.getElementById("jinrishici-sentence-quanwen");
+	 var content=break_line.innerHTML;
+	 break_line.innerHTML=content.replace(/。/g,"。<br>");  
+	 var content=break_line.innerHTML;
+	 break_line.innerHTML=content.replace(/,/g,""); 
+	 var content=break_line.innerHTML;
+	 break_line.innerHTML=content.replace(/！/g,"！<br>");
+	 var content=break_line.innerHTML;
+	 break_line.innerHTML=content.replace(/？/g,"？<br>");
+	/*var content=break_line.innerHTML;
+	 break_line.innerHTML=content.replace(/，/g,"，<br>");*/
+    });
